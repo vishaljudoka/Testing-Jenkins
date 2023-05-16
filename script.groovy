d5b45e294ad85ddaea3c18c889b6af10f9e0fbef
vishaljudoka <65101809+vishaljudoka@users.noreply.github.com>
2023-05-16 16:29:58 -0400

    11th commit

---
+++ script.groovy
@@ -0,0 +1,17 @@
+def buildApp() 
+	{
+	echo 'Building the applic]ation1'
+	}
+def testApp() 
+	{
+	echo 'Testing the application1'
+	}
+
+def deployApp() 
+	{
+	echo 'Deploying the applic]ation1'
+	}
+	
+return this
+	 
+	 
